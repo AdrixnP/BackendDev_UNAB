17b1280a9e774190b15cdbc0c0ab471bc10c7c8d
Hannd15 <lejhernandez08@gmail.com>
2024-02-27 20:18:08 -0500

    css 3

--- PHP/Luis_Seccion01.php
+++ PHP/Luis_Seccion01.php
@@ -1,19 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../CSS/index.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sección 01</title>
+</head>
+<body>
+    <?php
+    function cypherHalf1($i){
+        $sum = $i+7;
+        return $sum % 10;
+    }
+
+    $n = str_split($_POST["nInicial"]);
+    $n = array_map("cypherHalf1", $n);
+    $f[0] = $n[2];
+    $f[1] = $n[3];
+    $f[2] = $n[0];
+    $f[3] = $n[1];
+    print_r("El número cifrado es: ");
+    print_r(join("", $f));
+
+
+    ?>
+    </pre>
+    
+</body>
+</html>
 <pre>
-<?php
-function cypherHalf1($i){
-    $sum = $i+7;
-    return $sum % 10;
-}
-
-$n = str_split($_POST["nInicial"]);
-$n = array_map("cypherHalf1", $n);
-$f[0] = $n[2];
-$f[1] = $n[3];
-$f[2] = $n[0];
-$f[3] = $n[1];
-print_r("El número cifrado es:");
-print_r(join("", $f));
-
-
-?>
-</pre>
